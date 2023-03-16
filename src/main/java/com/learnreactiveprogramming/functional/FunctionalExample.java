1fc06bbbf04745c9d748aaa3bb2a3666a7e100e8
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2023-03-16 10:12:06 -0300

    fea: comparing imperative vs functional programming

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var nameList = List.of("alex","ben","chloe","adam","adam");
+        var newNameList = namesGreaterThanSize(nameList, 3);
+        System.out.println("newNameList: " + newNameList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> nameList, int size) {
+        return nameList.parallelStream()
+                .filter(s -> s.length() > size)
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,14 +1,24 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImperativeExample {
     public static void main(String[] args) {
-        var nameList = List.of("alex","ben","chloe","adam");
-        namesGreaterThanSize(nameList, 3);
+        var nameList = List.of("alex", "ben", "chloe", "adam", "adam");
+        var newNameList = namesGreaterThanSize(nameList, 3);
+        System.out.println("newNameList: " + newNameList);
     }
 
-    private static List<String> namesGreaterThanSize(List<String> nameList, int i) {
+    private static List<String> namesGreaterThanSize(List<String> nameList, int size) {
+        var newNameList = new ArrayList<String>();
 
+        for (String name : nameList) {
+            if (name.length() > size && !newNameList.contains(name)) {
+                newNameList.add(name.toUpperCase());
+            }
+        }
+
+        return newNameList;
     }
 }
