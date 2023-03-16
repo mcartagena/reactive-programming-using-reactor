78a5d97841a22b867d2247414776d176557ca080
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2023-03-16 09:53:56 -0300

    first commit

---
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -0,0 +1,14 @@
+package com.learnreactiveprogramming.imperative;
+
+import java.util.List;
+
+public class ImperativeExample {
+    public static void main(String[] args) {
+        var nameList = List.of("alex","ben","chloe","adam");
+        namesGreaterThanSize(nameList, 3);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> nameList, int i) {
+
+    }
+}
